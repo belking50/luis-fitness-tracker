4687b50ac29c99957aab5f215e319d124b077463
belking50 <nolgflow@gmail.com>
2026-07-28 14:31:59 -0400

    Add weekly workout calendar (#4)
    Co-authored-by: Nol SF <nolmuzik@Nols-MacBook-Pro-217.local>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "opto-elite-v3";
+const CACHE = "opto-elite-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
