033f4622c7da11109a7d566ddb1f3250dc3882d7
Nol SF <nolmuzik@Nols-MacBook-Pro-217.local>
2026-07-28 14:24:09 -0400

    Separate user data and detail workout plans

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "opto-elite-v2";
+const CACHE = "opto-elite-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
