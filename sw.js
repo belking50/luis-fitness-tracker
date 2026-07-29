aaeddc43f2e288eb680fe90e3cac9aff144c9178
belking50 <nolgflow@gmail.com>
2026-07-29 17:34:13 -0400

    Add Legacy 10-day workout library (#6)
    * Add Legacy 10-day workout library
    
    * Refresh PWA cache for Legacy workouts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "opto-elite-v5";
+const CACHE = "opto-elite-v6";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
