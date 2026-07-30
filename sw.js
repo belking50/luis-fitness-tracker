f4e03e1458ff7cdf932f177b4114abb2ab82a949
belking50 <nolgflow@gmail.com>
2026-07-29 21:10:26 -0400

    Add images to every Legacy exercise (#7)
    * Add images to Legacy exercises
    
    * Refresh PWA cache for exercise images
    
    Updated service worker to use a new cache version and improved fetch handling.
    
    * Fix duplicated service worker content
    
    Removed service worker caching logic and event listeners.
    
    * Fix duplicated index content

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "opto-elite-v6";
+const CACHE = "opto-elite-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
